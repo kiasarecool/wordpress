4c2e6c8197d60ef4e7da556cb8aa9614d88ab132
kiasarecool <92879803+kiasarecool@users.noreply.github.com>
2023-07-31 14:53:46 -0500

    Create dynamic-lgoin-logout-button.php

---
+++ Theme functions.php additions/dynamic-lgoin-logout-button.php
@@ -0,0 +1,12 @@
+<?php
+////dynamic log in/out button
+
+add_filter('wp_nav_menu_items', 'float_login_logout', 10, 2);
+function float_login_logout($items, $args) {
+        ob_start();
+        wp_loginout('/my-account/');
+        $loginoutlink = ob_get_contents();
+        ob_end_clean();
+        $items .= $loginoutlink .'</li>';
+    return $items;
+}
